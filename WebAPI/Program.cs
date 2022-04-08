3773c77fc5935ad8f86974d8dd6fb3091aaa04e8
İbrahim Fətəliyev <ibrahim.fataliyev.c@asoiu.edu.az>
2022-04-08 17:04:43 +0400

    Add project files.

---
+++ WebAPI/Program.cs
@@ -0,0 +1,40 @@
+using Buisness.Abstract;
+using Buisness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICategoryDal, CategoryDal>();
+builder.Services.AddScoped<ICategoryManager, CategoryManager>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//builder.Services.AddDefaultIdentity<K205User>().AddRoles<IdentityRole>()
+//    .AddEntityFrameworkStores<ShopContext>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
